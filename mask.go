0573fac53abdafca71cf67e1d92d798f56a446c4
Seth Ammons <seth.ammons@sendgrid.com>
2015-07-08 04:00:59 +0000

    now able to store numbers larger than 64

--- mask.go
+++ mask.go
@@ -1,39 +1,50 @@
 package idmask
 
-var TotalIDs uint = 150
+import "fmt"
 
-func Mask(ids []uint) int {
-	var mask int
+var MaxID uint64 = 150
+
+func Mask(ids []uint64) []uint64 {
+	mask := make([]uint64, uint64(MaxID)/64+1)
 	for _, id := range ids {
-		mask = setBit(mask, id)
+		mask[id/64] = setBit(mask[id/64], id%64)
 	}
 	return mask
 }
 
-func Unmask(n int) []uint {
-	var ids []uint
+func Unmask(n []uint64) []uint64 {
+	var ids []uint64
 
 	// not using 0 because this use case is for ids, and id 0 is just silly.
-	for i := uint(1); i <= TotalIDs; i++ {
-		if hasBit(n, i) {
-			ids = append(ids, i)
+	for i, idPool := range n {
+		for j := uint64(0); j < MaxID; j++ {
+			if hasBit(idPool, j) {
+				ids = append(ids, j+uint64(i*64))
+			}
 		}
 	}
 	return ids
 }
 
-func setBit(n int, pos uint) int {
+func setBit(n uint64, pos uint64) uint64 {
 	n |= (1 << pos)
 	return n
 }
 
-func clearBit(n int, pos uint) int {
-	mask := ^(1 << pos)
+func clearBit(n uint64, pos uint64) uint64 {
+	mask := uint64(^(1 << pos))
 	n &= mask
 	return n
 }
 
-func hasBit(n int, pos uint) bool {
+func hasBit(n uint64, pos uint64) bool {
 	val := n & (1 << pos)
 	return (val > 0)
+}
+
+func debug(format string, args ...interface{}) {
+	debug := false
+	if debug {
+		fmt.Printf(format+"\n", args...)
+	}
 }
--- mask_test.go
+++ mask_test.go
@@ -6,13 +6,13 @@ import (
 )
 
 func TestIDMask(t *testing.T) {
-	originalIDs := []uint{1, 3, 5, 8}
+	originalIDs := []uint64{1, 3, 5, 8}
 	bits := Mask(originalIDs)
 	btoi, err := strconv.ParseInt(reverse("010101001"), 2, 64)
 	if err != nil {
 		t.Fatalf("unable to converte binary to int - `%v`", err)
 	}
-	if got, want := bits, int(btoi); got != want {
+	if got, want := bits[0], uint64(btoi); got != want {
 		t.Errorf("got `%d` (%b), want `%d` (%b)", got, got, want, want)
 	}
 
@@ -23,21 +23,26 @@ func TestIDMask(t *testing.T) {
 }
 
 func TestIDMaskLarger(t *testing.T) {
-	originalIDs := []uint{1, 3, 5, 8, 140, 150, 6, 2, 62, 63, 63, 127, 128, 129}
+	// out of order, and numbers near multiples of 64
+	originalIDs := []uint64{1, 3, 5, 8, 140, 150, 6, 72, 62, 63, 64, 65, 127, 128, 129, 130}
 
 	bits := Mask(originalIDs)
 	restoredIDs := Unmask(bits)
+
+	if len(bits) != 3 {
+		t.Errorf("got %d bit pools, want %d", len(bits), 3)
+	}
 
 	if got, want := restoredIDs, originalIDs; !listEqual(got, want) {
 		t.Errorf("got %v, want %v", restoredIDs, originalIDs)
 	}
 }
 
-func listEqual(a, b []uint) bool {
+func listEqual(a, b []uint64) bool {
 	if len(a) != len(b) {
 		return false
 	}
-	hashA := make(map[uint]int)
+	hashA := make(map[uint64]int)
 
 	for _, element := range a {
 		if value, ok := hashA[element]; ok {
